f374fafc3af667474beb4dca45fc6a663f5abdbe
joseangelramirezrodriguez <angel.ramirezrodriguez@galileo.edu>
2026-03-15 08:11:10 +0000

    se suben los archivos a github

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hola, mundo, Soy Jose Angel Ramirez Rodriguez" ;
+ 
+?>
+<html> 
+    <br/>
+<img src="/proyecto_php/hmph.png" alt="loli rubia xd" width="200" height="200">
+
+
+</hmtl>
